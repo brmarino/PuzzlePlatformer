8f7710aeea38f698e693f5ad1bf907d87820f6ea
Marcus <marcus.casey@outlook.com>
2017-12-06 17:40:14 -0800

    final version, jumping and text bug, levels still needed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed;
 	public float jumpForce;
+	public Text countText;          //Store a reference to the UI Text component which will display the number of pickups collected.
+	public Text winText;            //Store a reference to the UI Text component which will display the 'You win' message.
+	private int count;              //Integer to store the number of pickups collected so far.
+
 
 	public KeyCode left;
 	public KeyCode right;
@@ -30,8 +36,13 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		theRB = GetComponent<Rigidbody2D>();
-
+		count = 0;
 		anim = GetComponent<Animator>();
+		//Initialze winText to a blank string since we haven't won yet at beginning.
+		winText.text = "";
+
+		//Call our SetCountText function which will update the text with the current value for count.
+		SetCountText ();
 	}
 	
 	// Update is called once per frame
@@ -74,4 +85,33 @@ public class PlayerController : MonoBehaviour {
 		anim.SetFloat("Speed", Mathf.Abs(theRB.velocity.x));
 		anim.SetBool("Grounded", isGrounded);
 	}
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		//Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+		if (other.gameObject.CompareTag("PickUp"))
+
+			//... then set the other object we just collided with to inactive.
+			other.gameObject.SetActive(false);
+
+		//Add one to the current value of our count variable.
+		count = count + 1;
+
+		//Update the currently displayed count by calling the SetCountText function.
+		SetCountText ();
+	}
+
+	//This function updates the text displaying the number of objects we've collected and displays our victory message if we've collected all of them.
+	void SetCountText()
+	{
+		//Set the text property of our our countText object to "Count: " followed by the number stored in our count variable.
+		countText.text = "Count: " + count.ToString ();
+
+		//Check if we've collected all 12 pickups. If we have...
+		if (count >= 3){
+			//... then set the text property of our winText object to "You win!"
+			winText.text = "You win!";
+			SceneManager.LoadScene("MenuScreen");
+
+	}
+}
 }
